4464bd95031e65b03be22724f499342ff1eb9848
Samuel-nilsson-nti-johanneberg <samuel.nilsson2@elev.ga.ntig.se>
2024-01-24 10:37:34 +0100

    lektionsarbete

--- Samuel/app.rb
+++ Samuel/app.rb
@@ -5,9 +5,62 @@ require 'sqlite3'
 
 
 
-get('/')  do
+  get('/')  do
     slim(:start)
-  end 
+  end
+
+  get('users/register') 
+    slim(:"register")
+
+  get('/showlogin') do
+    slim(:login)
+  end
+  
+  
+  post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new("db/todo2021.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/todos')
+    else
+      "Fel lösenord!"
+    end
+  end
+  
+  get('/todos') do
+    id = session[:id].to_i
+    db = SQLite3::Database.new("db/todo2021.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
+    p "Alla todos från result #{result}"
+    slim(:"todos/index",locals:{todos:result})
+  end
+  
+  post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new("db/todo2021.db")
+      db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)",username,password_digest)
+      redirect('/')
+    else
+      "Lösenorden matchar inte!"
+    end
+  end
+
+
+
+
   
   get('/albums') do
     db = SQLite3::Database.new("db/chinook-crud.db")
