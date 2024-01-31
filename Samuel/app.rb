ea61c1dd4b369eb26d4f56d5d04f496fa906027d
Samuel-nilsson-nti-johanneberg <samuel.nilsson2@elev.ga.ntig.se>
2024-01-31 10:25:55 +0100

    lektionsjobb

--- Samuel/app.rb
+++ Samuel/app.rb
@@ -9,61 +9,62 @@ require 'sqlite3'
     slim(:start)
   end
 
-  get('users/register') 
-    slim(:"register")
+#   get('users/register') 
+#     slim(:"register")
+#   end
 
-  get('/showlogin') do
-    slim(:login)
-  end
+  # get('/showlogin') do
+  #   slim(:login)
+  # end
   
   
-  post('/login') do
-    username = params[:username]
-    password = params[:password]
-    db = SQLite3::Database.new("db/todo2021.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
-    pwdigest = result["pwdigest"]
-    id = result["id"]
+  # post('/login') do
+  #   username = params[:username]
+  #   password = params[:password]
+  #   db = SQLite3::Database.new("db/musicsite.db")
+  #   db.results_as_hash = true
+  #   result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  #   pwdigest = result["pwdigest"]
+  #   id = result["id"]
     
-    if BCrypt::Password.new(pwdigest) == password
-      session[:id] = id
-      redirect('/todos')
-    else
-      "Fel lösenord!"
-    end
-  end
+  #   if BCrypt::Password.new(pwdigest) == password
+  #     session[:id] = id
+  #     redirect('/todos')
+  #   else
+  #     "Fel lösenord!"
+  #   end
+  # end
   
-  get('/todos') do
-    id = session[:id].to_i
-    db = SQLite3::Database.new("db/todo2021.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
-    p "Alla todos från result #{result}"
-    slim(:"todos/index",locals:{todos:result})
-  end
+  # get('/todos') do
+  #   id = session[:id].to_i
+  #   db = SQLite3::Database.new("db/musicsite.db")
+  #   db.results_as_hash = true
+  #   result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
+  #   p "Alla todos från result #{result}"
+  #   slim(:"todos/index",locals:{todos:result})
+  # end
   
-  post('/users/new') do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
+  # post('/users/new') do
+  #   username = params[:username]
+  #   password = params[:password]
+  #   password_confirm = params[:password_confirm]
   
-    if (password == password_confirm)
-      password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new("db/todo2021.db")
-      db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)",username,password_digest)
-      redirect('/')
-    else
-      "Lösenorden matchar inte!"
-    end
-  end
+  #   if (password == password_confirm)
+  #     password_digest = BCrypt::Password.create(password)
+  #     db = SQLite3::Database.new("db/musicsite.db")
+  #     db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)",username,password_digest)
+  #     redirect('/')
+  #   else
+  #     "Lösenorden matchar inte!"
+  #   end
+  # end
 
 
 
 
   
   get('/albums') do
-    db = SQLite3::Database.new("db/chinook-crud.db")
+    db = SQLite3::Database.new("db/musicsite.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM albums")
     p result
@@ -78,14 +79,14 @@ require 'sqlite3'
     title = params[:title]
     artist_id = params[:artist_id].to_i
     p "vi fick datan #{title} och #{artist_id}"
-    db = SQLite3::Database.new("db/chinook-crud.db")
+    db = SQLite3::Database.new("db/musicsite.db")
     db.execute("INSERT INTO albums (Title, ArtistId) VALUES (?,?)",title, artist_id)
     redirect('/albums')
   end
   
   post('/albums/:id/delete') do
     id = params[:id].to_i
-    db = SQLite3::Database.new("db/chinook-crud.db")
+    db = SQLite3::Database.new("db/musicsite.db")
     db.execute("DELETE FROM albums WHERE AlbumId = ?",id)
     redirect('/albums')
   end
@@ -94,14 +95,14 @@ require 'sqlite3'
     id = params[:id].to_i
     title = params[:title]
     artist_id = params[:artistId].to_i
-    db = SQLite3::Database.new("db/chinook-crud.db")
+    db = SQLite3::Database.new("db/musicsite.db")
     db.execute("UPDATE albums SET Title=?,ArtistId=? WHERE AlbumId = ?",title,artist_id,id)
     redirect('/albums')
   end
   
   get('/albums/:id/edit') do
     id = params[:id].to_i
-    db = SQLite3::Database.new("db/chinook-crud.db")
+    db = SQLite3::Database.new("db/musicsite.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
     slim(:"/albums/edit",locals:{result:result})
@@ -109,7 +110,7 @@ require 'sqlite3'
   
   get('/albums/:id') do
     id = params[:id].to_i
-    db = SQLite3::Database.new("db/chinook-crud.db")
+    db = SQLite3::Database.new("db/musicsite.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
     result2 = db.execute("SELECT Name FROM Artists WHERE ArtistId IN (SELECT ArtistId FROM Albums WHERE AlbumId = ?)",id).first
